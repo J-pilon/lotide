fa71d2143f136d638f84ebf71231120b305b5d1b
Josiah <josiah.pilon@yahoo.ca>
2021-03-04 20:15:44 +0000

    This eqObjects function checks to see if two objects are the same.

--- eqObjects.js
+++ eqObjects.js
@@ -6,6 +6,30 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+const eqArrays = function(array1, array2) {
+
+  let longestArray = [];
+  if (array1.length > array2.length) {
+    longestArray = array1;
+  } else {
+    longestArray = array2;
+  }
+  
+
+  for (let i = 0; i < longestArray.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+
+  for (let i = 0; i < longestArray.length; i++) {
+    if (array1[i] === array2[i]) {
+      return true;
+    }
+  }
+
+};
+
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 const eqObjects = function(object1, object2) {
@@ -17,30 +41,33 @@ const eqObjects = function(object1, object2) {
 
     for (const key of keysOf1){
 
-      console.log(key); 
+      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
 
-      if (object1[key] === object2[key]) {
+        if (!eqArrays(object1[key], object2[key])) {
 
-
-      } else {
-
+          return false;
+        };
+      } else if (object1[key] !== object2[key]) {
+      
         return false;
-      }
+        } 
     }
 
   } else {
+
     return false;
   }
 
+  return true;
   
 };
 
 const cd = { c: "1", d: ["2", 3] };
-// const dc = { d: ["2", 3], c: "1" };
+const dc = { d: ["2", 3], c: "1" };
 // assertEqual(eqObjects(cd, dc), true); // => true
 
-const cd2 = { c: "1", d: [2, 3, 4] };
+const cd2 = { c: "1", d: ["2", 3] };
 
-eqObjects(cd, cd2)
+eqObjects(cd, dc)
 
-// assertEqual(eqObjects(cd, cd2), false)
+assertEqual(eqObjects(cd, cd2), true)
