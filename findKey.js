9b1f9cb5d6139d7f715cc9fda0889af4a34b60a9
Josiah <josiah.pilon@yahoo.ca>
2021-03-05 04:57:03 +0000

    findKey function return the key when the callback returns truthy

---
+++ findKey.js
@@ -0,0 +1,26 @@
+//parameter one is an object
+// parameter two is a callback
+// for..in to loop through keys
+// if statement return true when you find the value that matches your callback
+
+const findKey = function(obj, callback) {
+
+  for (const property in obj) {
+
+    if (callback(obj[property])) {
+
+      return property;
+    }
+  }
+};
+
+const x = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2);
+
+console.log(x);
