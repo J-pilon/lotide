61bbaf8237648664d8d6f98314957e126e79d51a
Josiah <josiah.pilon@yahoo.ca>
2021-03-09 04:53:35 +0000

    refactored to require export middle function

--- middle.js
+++ middle.js
@@ -1,64 +1,25 @@
-const assertArraysEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const eqArrays = function(array1, array2) {
-
-  let longestArray = [];
-  if (array1.length > array2.length) {
-    longestArray = array1;
-  } else {
-    longestArray = array2;
-  }
-  
-
-  for (let i = 0; i < longestArray.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-
-  for (let i = 0; i < longestArray.length; i++) {
-    if (array1[i] === array2[i]) {
-      return true;
-    }
-  }
-
-};
-
 const middle = function(array) {
 
   let arrayCopy = [];
   let middleArray = [];
   let indexMiddle = 0;
 
-  for (let index in array) {
-    arrayCopy.push(array[index]);
+  for (let i = 0; i < array.length; i++) {
+    arrayCopy.push(array[i]);
   }
 
-  if (array.length === 2) {
-
+  if (arrayCopy.length === 2) {
+    return arrayCopy;
+  } else if (arrayCopy.length % 2 === 0) {
+    indexMiddle = (arrayCopy.length / 2) - 1;
+    middleArray = arrayCopy.splice(indexMiddle, 2);
     return middleArray;
 
-  } else if (array.length % 2 === 0) {
-
-    indexMiddle = (array.length / 2) - 1;
-
-    middleArray.push(arrayCopy.splice(indexMiddle,2));
-
-    return middleArray;
-
-  } else {
-    indexMiddle = (array.length - 1) / 2;
-
-    middleArray.push(arrayCopy.splice(indexMiddle, 1));
-
+  } else if (arrayCopy.length % 3 === 0) {
+    indexMiddle = (arrayCopy.length - 1) / 2;
+    middleArray = arrayCopy.splice(indexMiddle, 1);
     return middleArray;
   }
 };
 
-console.log(middle([1,2]));
+module.exports = middle;
