6ea7e9c2e6a464c07c52d2a2d52d8cad049da4ba
Josiah <josiah.pilon@yahoo.ca>
2021-03-10 00:58:50 +0000

    cleaned

--- countOnly.js
+++ countOnly.js
@@ -19,21 +19,7 @@ const countOnly = function(allItems, itemsToCount) {
   return results;
 };
 
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
 
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
-console.log(result1)
 
 module.exports = countOnly;
 
